603b78d96988f898b06979505503a73a12e2a962
Diego G <dcgaray01@gmail.com>
2022-02-12 15:23:49 -0800

    updating shell script
    let's get this bread

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-echo "Running main.py"
-
-if [ $# -eq 0 ]
+echo "~~~Running main.py~~~"
+#if no args supplied, it'll just run the code
+if [ $# -eq 0 ] 
     then
-        echo "No arguments supplied"
+        echo "~~~No arguments supplied~~~"
+        python3 main.py
 fi
-if [ $# -eq 1 ]
-    then
+
+#if given an output file name, the code will run and create a result file
+if [ $# -eq 1 ] 
+then
     python3 main.py > $1.out
-    echo "Results sent out to $1.out"
+    echo "~~~Results sent out to $1.out~~~"
 fi
 
-echo "Finished running!"
+echo "~~~Finished running!~~~"
 
 
