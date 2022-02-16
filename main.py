38374fb751f84d9528431fc186f250421b31aaaf
Diego G <dcgaray01@gmail.com>
2022-02-15 16:35:35 -0800

    error handling for program intialization
    okay, so now if we're given a file that doens't work we won't break, we exit gracefully

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from PIL import Image
+import sys
+
 
 def main():
     #Ex// cipher = AES.new([key], [mode])
-    pass
-aldkfjad
+    try:
+        inputF = sys.argv[1] 
+    except: 
+        print(f"Usage: main.py [filename].bmp")
+        return
+    try:
+        im = Image.open(inputF, mode="r")
+    except:
+        print(f"That is not a valid file.")
+        return 
+
+    print(im.info)
+
+
 
 if __name__ == "__main__":
     main()
