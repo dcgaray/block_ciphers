7c558d0a20767f01f4c5ca3cd74d2b5b50bdf09f
Diego G <dcgaray01@gmail.com>
2022-02-22 20:51:01 -0800

    submit() function done
    okay, so I went back to Sublime to make my life easier. I am getting a fantastic crash course into how Python is working

--- cbc.py
+++ cbc.py
@@ -1,11 +1,12 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-def CBC(info,cipher_key=get_random_bytes(16)):
+def CBC(info,cipher_key=get_random_bytes(16), iv=get_random_bytes(16)):
     ogLen = len(info) # we must save the original length of information
     blockLen = 16
     paddedInfo = pad(info,blockLen)
-    encryptedInfo = aesCbcEncryption(cipher_key, paddedInfo)
+    mode = AES.MODE_CBC
+    encryptedInfo = aesCbcEncryption(cipher_key, paddedInfo, mode, iv)
     return encryptedInfo
 
 # PKCS #7 specisies that the value of each added byte is the number of bytes that are added.
@@ -20,8 +21,7 @@ def pad(information,block_length):
 
 #takes a cipher key, and information to encrypt with ECB using AES
 #NOTE: the information being passed in must be padded
-def aesCbcEncryption(key, information, mode=AES.MODE_CBC):
-    iv = get_random_bytes(16)
+def aesCbcEncryption(key, information, mode=AES.MODE_CBC, iv=get_random_bytes(16)):
     aes = AES.new(key,mode,iv)
     new_info = aes.encrypt(information)
     return new_info
--- confid.py
+++ confid.py
@@ -1,7 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from cbc import CBC
-import urllib.parse
+import urllib.parse #god bless the Python gods for having a built-in function for just about anything
 
 iv = get_random_bytes(16)
 cipher_key = get_random_bytes(16)
@@ -16,13 +16,18 @@ def condifidentialityLimits():
 
 
 # str -> URL encoded and AES-128-CBC encrypted string
-def submit(userString):
+def submit(query):
     prependStr = "userid=456;userdata="
     appendStr = ";session-id=31337"
     # %3B is the URL encoding of ";"
     # %3D is the URL encoding of "="
-    urllib.parse.quote(userString)  # OKAY, WHEN PRINTING IT DOESN'T LOOK ANY DIFFERNT BUT IT"S BEEN URL-ENCODED = source;trust_me
+    # OKAY, WHEN PRINTING IT DOESN'T LOOK ANY DIFFERNT BUT IT"S BEEN URL-ENCODED = source;trust_me
+    urlEncodedQuery = urllib.parse.quote(query)  
 
+    #our CBC function expects our answer to be in bytes
+    bytesQuery = bytes(urlEncodedQuery, 'utf-8')
+    cbcEncodedQuery = CBC(bytesQuery, cipher_key, iv)
+    return cbcEncodedQuery 
 
 # PKCS #7 specisies that the value of each added byte is the number of bytes that are added.
 # AES has a fixed data block size of 16 bits. If our information is divisible by 16 bytes, then 
@@ -33,3 +38,4 @@ def pad(information, block_length):
     # 1 * k - (lth mod k) <= from RFC 5652 6.3
     pad = b"\x00" * (block_length - (info_len % block_length))
     return (information + pad)
+
