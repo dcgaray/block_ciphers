6288a11f82629b5697f85960a3c0862a32b65da5
Diego G <dcgaray01@gmail.com>
2022-02-22 19:56:52 -0800

    userString is now properly URL-encoded
    alright, this should be the start, and now i should just be able to PAD this and encode it with CBC

--- confid.py
+++ confid.py
@@ -1,5 +1,6 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+import urllib.parse
 
 iv = get_random_bytes(16)
 cipher_key = get_random_bytes(16)
@@ -13,12 +14,18 @@ def condifidentialityLimits():
 
 
 
-#str -> whatever
-def submit(username): 
+#str -> URL encoded and AES-128-CBC encrypted string
+def submit(userString): 
     prependStr="userid=456;userdata="
     appendStr=";session-id=31337"
-    information = prependStr + username + appendStr
-    return information
+    #%3B is the URL encoding of ";"
+    #%3D is the URL encoding of "="
+    urllib.parse.quote(userString) #OKAY, WHEN PRINTING IT DOESN'T LOOK ANY DIFFERNT BUT IT"S BEEN URL-ENCODED = source;trust_me
+
+    #information = prependStr + userString + appendStr
+
+    return "hahaha" 
+    #return information
 
 # PKCS #7 specisies that the value of each added byte is the number of bytes that are added.
 # AES has a fixed data block size of 16 bits. If our information is divisible by 16 bytes, then 
