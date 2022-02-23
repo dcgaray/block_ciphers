a3b316d8ad5f0694da5fa171e53516c39cc61933
Diego G <dcgaray01@gmail.com>
2022-02-22 21:21:49 -0800

    verify() done
    alright, now time to break it

--- cbc.py
+++ cbc.py
@@ -6,7 +6,7 @@ def CBC(info,cipher_key=get_random_bytes(16), iv=get_random_bytes(16)):
     blockLen = 16
     paddedInfo = pad(info,blockLen)
     mode = AES.MODE_CBC
-    encryptedInfo = aesCbcEncryption(cipher_key, paddedInfo, mode, iv)
+    encryptedInfo = aesCbcEncryption(cipher_key, paddedInfo, iv, mode)
     return encryptedInfo
 
 # PKCS #7 specisies that the value of each added byte is the number of bytes that are added.
@@ -16,12 +16,12 @@ def pad(information,block_length):
     info_len = len(information)
     # b"\x00 is da number 1"
     # 1 * k - (lth mod k) <= from RFC 5652 6.3
-    pad = b"\x00" * (block_length - (info_len%block_length))
+    pad = b"\x00" * (block_length - (info_len % block_length))
     return (information + pad)
 
 #takes a cipher key, and information to encrypt with ECB using AES
 #NOTE: the information being passed in must be padded
-def aesCbcEncryption(key, information, mode=AES.MODE_CBC, iv=get_random_bytes(16)):
+def aesCbcEncryption(key, information, iv, mode=AES.MODE_CBC):
     aes = AES.new(key,mode,iv)
     new_info = aes.encrypt(information)
     return new_info
--- confid.py
+++ confid.py
@@ -1,41 +1,44 @@
+import urllib.parse #god bless the Python gods for having a built-in function for just about anything
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from cbc import CBC
-import urllib.parse #god bless the Python gods for having a built-in function for just about anything
 
-iv = get_random_bytes(16)
-cipher_key = get_random_bytes(16)
+
 
 
 def condifidentialityLimits():
     block_length = 16
-    inputStr = input("Username: ")
-    paddedStr = submit(inputStr)
-    # TODO : FINISH THIS
-    print(paddedStr)
+    intial_iv = get_random_bytes(16)
+    intial_cipher_key = get_random_bytes(16)
+    inputQuery = input("Username: ")
+    encodedQuery = submit(inputQuery, intial_cipher_key, intial_iv)
+    print(encodedQuery)
+    isAdmin =verify(encodedQuery, intial_cipher_key, intial_iv)
+    print(isAdmin)
 
 
 # str -> URL encoded and AES-128-CBC encrypted string
-def submit(query):
+def submit(query,cipher_key,iv):
     prependStr = "userid=456;userdata="
     appendStr = ";session-id=31337"
     # %3B is the URL encoding of ";"
     # %3D is the URL encoding of "="
     # OKAY, WHEN PRINTING IT DOESN'T LOOK ANY DIFFERNT BUT IT"S BEEN URL-ENCODED = source;trust_me
     urlEncodedQuery = urllib.parse.quote(query)  
-
     #our CBC function expects our answer to be in bytes
-    bytesQuery = bytes(urlEncodedQuery, 'utf-8')
+    bytesQuery = bytes(urlEncodedQuery, 'UTF-8')
+    #our CBC function naturally uses a PKCS #7 padding
     cbcEncodedQuery = CBC(bytesQuery, cipher_key, iv)
     return cbcEncodedQuery 
 
-# PKCS #7 specisies that the value of each added byte is the number of bytes that are added.
-# AES has a fixed data block size of 16 bits. If our information is divisible by 16 bytes, then 
-# we will add an extra block of bytes for padding
-def pad(information, block_length):
-    info_len = len(information)
-    # b"\x00 is da number 1"
-    # 1 * k - (lth mod k) <= from RFC 5652 6.3
-    pad = b"\x00" * (block_length - (info_len % block_length))
-    return (information + pad)
+def verify(encodedQuery, c_key, ivec):
+    #reuse our intial cipher_key an intialization vector
+    cipher = AES.new(c_key, AES.MODE_CBC, ivec)
 
+    #bask in the glory that is python.....
+    plaintext = cipher.decrypt(encodedQuery).decode('UTF-8')
+
+    #search the plaintext string for admin priviledges
+    #HINT: since all ";" & "=" are URL encoded, they don't show up, theoritcally impossible to make this function return true
+    res = ";admin=true;" in  plaintext
+    return res
