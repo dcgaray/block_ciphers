8157aad8e1133e4ad383a5895a3acf90ded4b3b2
Diego G <dcgaray01@gmail.com>
2022-03-09 17:53:22 -0800

    savepoint
    i gotta temporarily shelve this, will come back to it later

--- cbc.py
+++ cbc.py
@@ -1,9 +1,10 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+blockLen = 16
+
 def CBC(info,cipher_key=get_random_bytes(16), iv=get_random_bytes(16)):
     ogLen = len(info) # we must save the original length of information
-    blockLen = 16
     paddedInfo = pad(info,blockLen)
     mode = AES.MODE_CBC
     encryptedInfo = aesCbcEncryption(cipher_key, paddedInfo, iv, mode)
@@ -19,15 +20,24 @@ def pad(information,block_length):
     pad = b"\x00" * (block_length - (info_len % block_length))
     return (information + pad)
 
+
+#takes in two bytes strings and XORs all the bits together
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
 #takes a cipher key, and information to encrypt with ECB using AES
 #NOTE: the information being passed in must be padded
 def aesCbcEncryption(key, information, iv, mode=AES.MODE_CBC):
-    aes = AES.new(key,mode,iv)
+    new_info = b""
+    encBlk = iv 
+
+    for i in range(0,len(information), blockLen):
+        aes = AES.new(key,mode,encBlk)
+        blk = information[i:i+blockLen] 
+        xorBlk = byte_xor(encBlk,blk)
+        encBlk = aes.encrypt(xorBlk)
+        new_info += encBlk 
 
 
-    #take stevens advice and do this yourself, using every resulting ciphertext to XOR the next
-    ####Yeah this is cool, but like you're not doing it yourself
-
-
-    new_info = aes.encrypt(information)
+    #new_info = aes.encrypt(information)
     return new_info
--- main.py
+++ main.py
@@ -10,18 +10,26 @@ from confid import condifidentialityLimits
 def main():
     block_length = AES.block_size 
     cipher_key = get_random_bytes(16)    
+    iv = get_random_bytes(16)    
 
     inputQuery = input("message: ") 
     #our ECB function expects our answer to be in bytes
     bytesQuery = bytes(inputQuery, 'UTF-8')
+
+    '''
     ecbQuery = ECB(bytesQuery,cipher_key)
-    print(ecbQuery)
-    print(type(ecbQuery))
     cipher = AES.new(cipher_key, AES.MODE_ECB)
     plaintext = cipher.decrypt(ecbQuery).decode('utf-8')
     print(plaintext)
+    '''
 
-    ''' 
+    cbcQuery = CBC(bytesQuery, cipher_key, iv)
+    print(f"{cbcQuery} <- text and then it's a {type(cbcQuery)}")
+    cipher = AES.new(cipher_key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(cbcQuery).decode('utf-8')
+    print(plaintext)
+
+    '''
     if len(sys.argv) >= 2:
         infile = sys.argv[1]
         task1(infile) 
