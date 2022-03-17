9dc205b8d9d21c36349f4a1956a0c1b0c089948e
Diego G <dcgaray01@gmail.com>
2022-03-17 12:24:39 -0700

    getting rid of testing scaffolding
    okay, so i never got rid of this when I was fixing the code...

--- main.py
+++ main.py
@@ -11,30 +11,11 @@ def main():
     block_length = AES.block_size 
     cipher_key = get_random_bytes(16)    
     iv = get_random_bytes(16)    
-
-    inputQuery = input("message: ") 
-    #our ECB function expects our answer to be in bytes
-    bytesQuery = bytes(inputQuery, 'UTF-8')
-
-    '''
-    ecbQuery = ECB(bytesQuery,cipher_key)
-    cipher = AES.new(cipher_key, AES.MODE_ECB)
-    plaintext = cipher.decrypt(ecbQuery).decode('utf-8')
-    print(plaintext)
-    '''
-
-    cbcQuery = CBC(bytesQuery, cipher_key, iv)
-    print(f"{cbcQuery} <- text and then it's a {type(cbcQuery)}")
-    cipher = AES.new(cipher_key, AES.MODE_CBC, iv)
-    plaintext = cipher.decrypt(cbcQuery).decode('utf-8')
-    print(plaintext)
-
-    '''
+    
     if len(sys.argv) >= 2:
         infile = sys.argv[1]
         task1(infile) 
     task2()
-    '''
 
 def task1(infile):
     #Task1
